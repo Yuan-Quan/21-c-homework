37d42c11cada6129956c18572dd3708c117c104d
Cirno <cirno@MistyLake.localdomain>
2022-01-10 09:15:14 +0800

    updated chapter 3

--- 03_rational/big_int.c
+++ 03_rational/big_int.c
@@ -4,11 +4,11 @@
 
 // -----------------------------------------------------------------------------
 
-static BigInt* get_big_int(void)
+static BigInt *get_big_int(void)
 {
-    BigInt* bi = malloc(sizeof(BigInt));
+    BigInt *bi = malloc(sizeof(BigInt));
     bi->sign = plus;
-    bi->val  = 0;
+    bi->val = 0;
     return bi;
 }
 
@@ -16,26 +16,26 @@ static BigInt* get_big_int(void)
 
 // -----------------------------------------------------------------------------
 
-BigInt* big_int_from_ll(long long x)
+BigInt *big_int_from_ll(long long x)
 {
-    BigInt* bi = get_big_int();
+    BigInt *bi = get_big_int();
     bi->sign = (x >= 0) ? plus : minus;
-    bi->val  = unsigned_from_ll(bi->sign * x);
+    bi->val = unsigned_from_ll(bi->sign * x);
     return bi;
 }
 
-BigInt* big_int_copy(const BigInt* x)
+BigInt *big_int_copy(const BigInt *x)
 {
     if (!x)
         return 0;
 
-    BigInt* y = get_big_int();
+    BigInt *y = get_big_int();
     y->sign = x->sign;
-    y->val  = unsigned_copy(x->val);
+    y->val = unsigned_copy(x->val);
     return y;
 }
 
-void big_int_free(BigInt* bi)
+void big_int_free(BigInt *bi)
 {
     if (!bi)
         return;
@@ -44,7 +44,7 @@ void big_int_free(BigInt* bi)
     free(bi);
 }
 
-char* big_int_to_str(const BigInt* bi)
+char *big_int_to_str(const BigInt *bi)
 {
     if (!(bi && bi->val->size > 0))
         return 0;
@@ -52,45 +52,158 @@ char* big_int_to_str(const BigInt* bi)
     Int n = (bi->sign == minus) ? bi->val->size + 2
                                 : bi->val->size + 1;
 
-    char* ret = calloc(n, sizeof(char));
-    char* p   = ret;
+    char *ret = calloc(n, sizeof(char));
+    char *p = ret;
     if (bi->sign == minus)
         *p++ = '-';
 
-    for (Link* link = bi->val->head; link; ++p, link = link->next)
+    for (Link *link = bi->val->head; link; ++p, link = link->next)
         *p = link->n + '0';
     *p = '\0';
     return ret;
 }
 
-long long big_int_to_ll(const BigInt* bi)
+long long big_int_to_ll(const BigInt *bi)
 {
     return bi->sign * unsigned_to_ll(bi->val);
 }
 
-int big_int_cmp(const BigInt* x, const BigInt* y)
+int big_int_cmp(const BigInt *x, const BigInt *y)
 {
     // 在此处补充完整
+    // positive always bigger than negative
+    if (x->sign != y->sign)
+    {
+        if (x->sign == plus)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        if (x->sign == plus)
+        {
+            return unsigned_cmp(x->val, y->val);
+        }
+        else
+        {
+            return -1 * unsigned_cmp(x->val, y->val);
+        }
+    }
 }
 
-BigInt* big_int_add(const BigInt* x, const BigInt* y)
+BigInt *big_int_add(const BigInt *x, const BigInt *y)
 {
     // 在此处补充完整
+    BigInt *result = get_big_int();
+    // sign is different,
+    if (x->sign != y->sign)
+    {
+        if (x->sign == plus)
+        {
+            switch (unsigned_cmp(x->val, y->val))
+            {
+            case 0:
+            case 1:
+                result->val = unsigned_sub(x->val, y->val);
+                result->sign = plus;
+                break;
+            case -1:
+                result->val = unsigned_sub(y->val, x->val);
+                result->sign = minus;
+                break;
+            }
+        }
+        else
+        {
+            switch (unsigned_cmp(x->val, y->val))
+            {
+            case 0:
+            case 1:
+                result->val = unsigned_sub(x->val, y->val);
+                result->sign = minus;
+                break;
+            case -1:
+                result->val = unsigned_sub(y->val, x->val);
+                result->sign = plus;
+                break;
+            }
+        }
+    }
+    else
+    {
+        // if they have same sign
+        result->sign = x->sign;
+        result->val = unsigned_add(x->val, y->val);
+    }
+    return result;
 }
 
-BigInt* big_int_sub(const BigInt* x, const BigInt* y)
+BigInt *big_int_sub(const BigInt *x, const BigInt *y)
 {
     // 在此处补充完整
+    // get a copy of y
+    BigInt *flipped_y = big_int_copy(y);
+    // flip it
+    if (flipped_y->sign == plus)
+    {
+        flipped_y->sign = minus;
+    }
+    else
+    {
+        flipped_y->sign = plus;
+    }
+
+    return big_int_add(x, flipped_y);
 }
 
-BigInt* big_int_mul(const BigInt* x, const BigInt* y)
+BigInt *big_int_mul(const BigInt *x, const BigInt *y)
 {
     // 在此处补充完整
+    BigInt *result = get_big_int();
+    if (x->sign != y->sign)
+    {
+        result->sign = minus;
+    }
+    else
+    {
+        result->sign = plus;
+    }
+
+    result->val = unsigned_mul(x->val, y->val);
+
+    return result;
 }
 
-BigInt* big_int_div(const BigInt* x, const BigInt* y, BigInt** rem)
+BigInt *big_int_div(const BigInt *x, const BigInt *y, BigInt **rem)
 {
     // 在此处补充完整
+    BigInt *result = get_big_int();
+    Unsigned *remainder = unsigned_from_ll(0);
+    if (!*rem)
+    {
+        *rem = get_big_int();
+    }
+
+    if (x->sign != y->sign)
+    {
+        result->sign = minus;
+    }
+    else
+    {
+        result->sign = plus;
+    }
+
+    result->val = unsigned_div(x->val, y->val, &remainder);
+
+    (*rem)->sign = x->sign;
+    (*rem)->val = remainder;
+
+    return result;
 }
 
 // -----------------------------------------------------------------------------
--- 03_rational/unsigned.c
+++ 03_rational/unsigned.c
@@ -8,11 +8,92 @@ static const Int ignore = 42;
 
 // 在此处可定义若干辅助函数，以供定义其它函数时使用
 
+static const Int epsilon = 10e-6;
+
+// return the n value of link, return 0 if it is NULL
+int get_link_value(Link *link)
+{
+    if (link == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return link->n;
+    }
+}
+
+// return 1 if x >= y, otherwise 0
+int unsigned_is_greater_than(const Unsigned *x, const Unsigned *y)
+{
+    if (x->size < y->size) // if y have more digit than x (thus greater than x), abort
+    {
+        return 0;
+    }
+    else if (x->size == y->size)
+    {
+        Link *ptrx = x->head;
+        Link *ptry = y->head; // if y is greater than x , abort
+        while (ptrx != 0)
+        {
+            if (ptry->n > ptrx->n)
+            {
+                return 0;
+            }
+            else if (ptrx->n > ptry->n)
+            {
+                break;
+            }
+
+            ptrx = ptrx->next;
+            ptry = ptry->next;
+        }
+    }
+    return 1;
+}
+
+int unsigned_is_equal(const Unsigned *x, const Unsigned *y)
+{
+    // if two list is not the same size, no need to compare
+    if (x->size != y->size)
+    {
+        return 0;
+    }
+
+    int flag = 1;
+    Link *ptrx = x->head;
+    Link *ptry = y->head;
+
+    while (flag && ptrx != 0 && ptry != 0) // keeep comparing and update flag, until something goes wrong
+    {
+        flag = ptrx->n == ptry->n;
+        ptrx = ptrx->next;
+        ptry = ptry->next;
+    }
+
+    return flag;
+}
+// remove the leading 0s
+void unsigned_remove_leading_zero(Unsigned *list)
+{
+    if (list->size == 0)
+    {
+        return;
+    }
+
+    Link *ptr = list->head;
+
+    while (ptr && ptr->n == 0)
+    {
+        ptr = ptr->next;
+        remove_front(list);
+    }
+}
 // -----------------------------------------------------------------------------
 
-Unsigned* unsigned_from_ll(long long x)
+Unsigned *unsigned_from_ll(long long x)
 {
-    Unsigned* ret = get_list();
+    Unsigned *ret = get_list();
 
     insert_front(ret, x % 10, ignore);
     while (x /= 10)
@@ -20,55 +101,207 @@ Unsigned* unsigned_from_ll(long long x)
     return ret;
 }
 
-Unsigned* unsigned_copy(const Unsigned* x)
+Unsigned *unsigned_copy(const Unsigned *x)
 {
     if (!x)
         return 0;
 
-    Unsigned* y = get_list();
-    for (Link* link = x->head; link; link = link->next)
+    Unsigned *y = get_list();
+    for (Link *link = x->head; link; link = link->next)
         insert_back(y, link->n, link->x);
     return y;
 }
 
-void unsigned_free(Unsigned* x)
+void unsigned_free(Unsigned *x)
 {
     list_free(x);
 }
 
-long long unsigned_to_ll(const Unsigned* x)
+long long unsigned_to_ll(const Unsigned *x)
 {
     long long ret = 0;
-    for (Link* link = x->head; link; link = link->next) {
+    for (Link *link = x->head; link; link = link->next)
+    {
         ret *= 10;
         ret += link->n;
     }
     return ret;
 }
 
-int unsigned_cmp(const Unsigned* x, const Unsigned* y)
+int unsigned_cmp(const Unsigned *x, const Unsigned *y)
 {
     // 在此处补充完整
+    if (unsigned_is_greater_than(x, y))
+    {
+        if (unsigned_is_equal(x, y))
+        {
+            return 0;
+        }
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
-Unsigned* unsigned_add(const Unsigned* x, const Unsigned* y)
+Unsigned *unsigned_add(const Unsigned *x, const Unsigned *y)
 {
     // 在此处补充完整
+    int carry_bit = 0;
+    Link *ptrx = x->tail;
+    Link *ptry = y->tail;
+    Unsigned *result = get_list();
+    int sum = 0;
+
+    while (ptrx || ptry || carry_bit)
+    {
+        sum = get_link_value(ptrx) + get_link_value(ptry);
+        if (carry_bit)
+        {
+            sum++;
+            carry_bit = 0;
+        }
+
+        insert_front(result, sum % 10, ignore);
+        if (sum >= 10)
+        {
+            carry_bit = 1;
+        }
+        if (ptrx != 0)
+        {
+            ptrx = ptrx->prior;
+        }
+        if (ptry != 0)
+        {
+            ptry = ptry->prior;
+        }
+    }
+    return result;
 }
 
-Unsigned* unsigned_sub(const Unsigned* x, const Unsigned* y)
+Unsigned *unsigned_sub(const Unsigned *x, const Unsigned *y)
 {
-    // 在此处补充完整
+    if (!unsigned_is_greater_than(x, y))
+    {
+        return NULL; // abort if y is greater than x
+    }
+
+    Unsigned *result = get_list();
+    Link *ptrx = x->tail;
+    Link *ptry = y->tail;
+    int sub = 0;
+    int borrow_bit = 0;
+    // we will do it from tail to head
+
+    while (ptrx || ptry)
+    {
+        sub = get_link_value(ptrx) - get_link_value(ptry);
+        if (borrow_bit)
+        {
+            sub--;
+            borrow_bit = 0;
+        }
+
+        if (sub < 0)
+        {
+            borrow_bit = 1;
+            sub += 10;
+        }
+
+        insert_front(result, sub, ignore);
+
+        if (ptrx != 0)
+        {
+            ptrx = ptrx->prior;
+        }
+        if (ptry != 0)
+        {
+            ptry = ptry->prior;
+        }
+    }
+
+    unsigned_remove_leading_zero(result);
+    return result;
 }
 
-Unsigned* unsigned_mul(const Unsigned* x, const Unsigned* y)
+Unsigned *unsigned_mul(const Unsigned *x, const Unsigned *y)
 {
     // 在此处补充完整
+    int carry_bit = 0;
+    Link *ptrx = x->tail;
+    Link *ptry = y->tail;
+    Unsigned *result = get_list();
+    Unsigned *mul = get_list();
+    int product = 0;
+
+    // keep track of how many 10s should be multiplied
+    int count = 0;
+    // for each digit of y, multiply with x, multiply some number of 10s, add to result
+    while (ptry)
+    {
+        // reset the mul for next round of calculation
+        list_free(mul);
+        mul = get_list();
+        ptrx = x->tail;
+
+        // calculate x * a digit of y
+        while (ptrx || carry_bit)
+        {
+            product = get_link_value(ptrx) * get_link_value(ptry);
+            if (carry_bit)
+            {
+                product += carry_bit;
+                carry_bit = 0;
+            }
+
+            insert_front(mul, product % 10, ignore);
+            if (product >= 10)
+            {
+                carry_bit = product / 10;
+            }
+            if (ptrx != 0)
+            {
+                ptrx = ptrx->prior;
+            }
+        }
+
+        // *10's, add with result
+        for (size_t i = 0; i < count; i++)
+        {
+            insert_back(mul, 0, ignore);
+        }
+        result = unsigned_add(result, mul);
+
+        // keep track of how much 10s should be multiplied
+        count++;
+
+        // increment ptry to next digit
+        if (ptry != 0)
+        {
+            ptry = ptry->prior;
+        }
+    }
+
+    return result;
 }
 
-Unsigned* unsigned_div(const Unsigned* x, const Unsigned* y, Unsigned** rem)
+Unsigned *unsigned_div(const Unsigned *x, const Unsigned *y, Unsigned **rem)
 {
     // 在此处补充完整
+    Unsigned *result = get_list();
+    Unsigned *one = get_list();
+    insert_front(one, 1, ignore);
+
+    while (unsigned_is_greater_than(x, y))
+    {
+        x = unsigned_sub(x, y);
+        result = unsigned_add(result, one);
+    }
+
+    *rem = x;
+
+    return result;
 }
 
 // -----------------------------------------------------------------------------
